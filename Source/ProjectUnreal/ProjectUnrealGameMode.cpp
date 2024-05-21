5c421741d3b0c6c733af3d875324ea1d480c35e2
Jari Bou <bourdietome@gmail.com>
2024-05-21 22:30:22 +0200

    stuff

--- Source/ProjectUnreal/ProjectUnrealGameMode.cpp
+++ Source/ProjectUnreal/ProjectUnrealGameMode.cpp
@@ -7,9 +7,12 @@
 AProjectUnrealGameMode::AProjectUnrealGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/_project/BP/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/_project/BP/BP_ThirdFirstPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+	// if (PlayerPawnBPClass.Class != NULL)
+	// {
+	// }
 }
--- Source/ProjectUnreal/ProjectUnrealGameMode.h
+++ Source/ProjectUnreal/ProjectUnrealGameMode.h
@@ -13,6 +13,7 @@ class AProjectUnrealGameMode : public AGameModeBase
 
 public:
 	AProjectUnrealGameMode();
+	
 };
 
 
